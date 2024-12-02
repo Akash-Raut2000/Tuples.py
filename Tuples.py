b750b784c93ca02474e0536746e8ddb253fa4353
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-02 14:04:16 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,11 @@
+# Empty tuple
+my_tuple = ()
+
+# Tuple with elements
+my_tuple = (1, 2, 3)
+
+# Tuple with mixed data types
+my_tuple = (1, "hello", 3.14)
+
+# Tuple without parentheses (optional when values are comma-separated)
+my_tuple = 1, 2, 3
